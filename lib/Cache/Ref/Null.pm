8d3983fc17e8e7df9f801cf0012e2bbc3cdccb1e
Yuval Kogman <nothingmuch@woobling.org>
2010-07-03 19:58:43 +0300

    add Null (allows comparing with no caching)

---
+++ lib/Cache/Ref/Null.pm
@@ -0,0 +1,54 @@
+package Cache::Ref::Null;
+use Moose;
+
+use namespace::autoclean;
+
+extends qw(Cache::Ref);
+
+with qw(Cache::Ref::Role::API);
+
+sub get { return }
+sub hit { return }
+sub set { return }
+sub remove { return }
+sub clear { return }
+sub expire { return }
+sub compute { return }
+
+__PACKAGE__->meta->make_immutable;
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Cache::Ref::Null - Caches nothing
+
+=head1 SYNOPSIS
+
+    # useful for comparing the effect of a cache compared to no
+    # caching without code changes:
+
+    my $c = Cache::Profile::Compare->new(
+        caches => [
+            Cache::Ref::Foo->new( ... ),
+            Cache::Ref->Null->new,
+        ],
+    );
+
+=head1 DESCRIPTION
+
+This cache implementation will cache nothing.
+
+This is primarily intended for testing or comparing runtime
+without a cache against runtime with a cache.
+
+It's like L<Cache::Null> but supports the additional methods in
+L<Cache::Ref>.
+
+=cut
+
+# ex: set sw=4 et:
